ee910b0aa6edb5d4bba762126720197622a2a646
Tiago Alcantara <tiagofacampos@gmail.com>
2021-08-18 15:57:20 -0300

    feat(semana-2): buscar categoria por id

--- src/main/java/br/com/tiagoalcantara/aluraflix/categories/controller/CategoryController.java
+++ src/main/java/br/com/tiagoalcantara/aluraflix/categories/controller/CategoryController.java
@@ -3,10 +3,13 @@ package br.com.tiagoalcantara.aluraflix.categories.controller;
 import br.com.tiagoalcantara.aluraflix.categories.dto.CategoryResponse;
 import br.com.tiagoalcantara.aluraflix.categories.model.Category;
 import br.com.tiagoalcantara.aluraflix.categories.repository.CategoryRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -25,6 +28,15 @@ public class CategoryController {
         List<Category> categoryList = categoryRepository.findAll();
         List<CategoryResponse> response = CategoryResponse.convertCategoryListToCategoryResponseList(categoryList);
 
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponse> find(@PathVariable Long id){
+        Category category = categoryRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "categoria não encontrada"));
+
+        CategoryResponse response = new CategoryResponse(category);
         return ResponseEntity.ok(response);
     }
 }
